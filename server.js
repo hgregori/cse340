e8a056835a621f696cfc11a856dfa25be9e8e9c9
Hgregori <Hgregorio@byupathway.edu>
2026-07-27 20:31:56 -0300

    W4-LA2

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
+import session from 'express-session';
+
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -13,8 +15,21 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
 const app = express();
 
+
+// Set up session management
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+// Use flash message middleware
+app.use(flash);
 /**
   * Configure Express middleware
   */
--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -26,11 +26,14 @@ const showNewOrganizationForm = async (req, res) => {
 
 const processNewOrganizationForm = async (req, res) => {
     const { name, description, contactEmail } = req.body;
-    const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations
+    const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations    
 
     const organizationId = await createOrganization(name, description, contactEmail, logoFilename);
+    
+    // Set a success flash message
+    req.flash('success', 'Organization added successfully!');
+    
     res.redirect(`/organization/${organizationId}`);
-};
 
 // Export any controller functions
 export { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm };
