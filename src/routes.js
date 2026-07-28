cb5108d7f53d74983e964ad60107e0c579bf0a36
Hgregori <Hgregorio@byupathway.edu>
2026-07-27 21:41:12 -0300

    W4-LA5

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import {
     processEditOrganizationForm
 } from './controllers/organizations.js';
 
-import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
+import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
@@ -44,6 +44,11 @@ router.post('/new-organization', organizationValidation, processNewOrganizationF
 router.get('/edit-organization/:id', showEditOrganizationForm);
 // Route to handle the edit organization form submission
 router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
+
+// Route for new project page
+router.get('/new-project', showNewProjectForm); 
+// Route to handle new project form submission
+router.post('/new-project', projectValidation, processNewProjectForm);
 
 
 export default router;
